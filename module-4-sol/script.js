c0e06e24ce774d1d306a0b442bd2178f500069f3
ronageorgem <81295162+ronageorgem@users.noreply.github.com>
2021-06-16 23:27:08 +0530

    updated

--- module-4-sol/script.js
+++ module-4-sol/script.js
@@ -1,6 +1,6 @@
 (function () {
 
-var names = ["Yaakov", "John", "jen", "Jason", "Paul", "frank", "Larry", "Paula", "Laura", "Jim"];
+var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
 for (var i = 0; i < names.length; i++) {
   var firstLetter = names[i].charAt(0).toLowerCase();
