6ad94409ab405604ce0679b4a4ed3b3beb3e0327
ronageorgem <81295162+ronageorgem@users.noreply.github.com>
2021-06-16 23:16:45 +0530

    updated

---
+++ module-4-sol/script.js
@@ -0,0 +1,17 @@
+(function () {
+
+var names = ["Yaakov", "John", "jen", "Jason", "Paul", "frank", "Larry", "Paula", "Laura", "Jim"];
+
+for (var i = 0; i < names.length; i++) {
+  var firstLetter = names[i].charAt(0).toLowerCase();
+
+  if (firstLetter === 'j') 
+  {
+    byeSpeaker.speak(names[i]);
+  } 
+  else 
+  {
+    helloSpeaker.speak(names[i]);
+  }
+}
+})();
---
+++ module-4-sol/speakgoodbye.js
@@ -0,0 +1,10 @@
+(function(){
+	var byeSpeaker = {};
+
+var speakWord = "Goodbye";
+byeSpeaker.speak = function (name) {
+  		console.log(speakWord + " " + name);
+	}
+
+window.byeSpeaker = byeSpeaker;
+})();
